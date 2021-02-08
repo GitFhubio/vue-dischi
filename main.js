5cd338947e37be75f98a52d9f9c2f20cac2ff3bd
Fabio Petrone <fa610movic90@gmail.com>
2021-02-08 13:35:25 +0100

    versione brutta

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
 const app = new Vue( {
     el: '#app',
     data: {
-       selected: '',
+       selected: 'all',
        listCds: [],
+       listfiltered:[]
     },
     mounted () {
         axios.get('https://flynn.boolean.careers/exercises/api/array/music')
         .then( response => {
             console.log(response.data.response);
             this.listCds = response.data.response;
-        })
-        .catch( error => {
-            console.log(error);
-        });
-    },
-methods:{
-filterGenre(){
-      this.listCds = this.listCds.filter( cd => cd.genre.toLowerCase() == this.selected);}
-    }
+            this.listfiltered = this.listCds.filter( cd => cd.genre.toLowerCase() == this.selected);
+     if(this.selected=='all'){
+       this.listfiltered=this.listCds;
+     }})
+
+  },
+    methods:{
+     filterGenre(){
+                   this.listfiltered = this.listCds.filter( cd => cd.genre.toLowerCase() == this.selected);
+            if(this.selected=='all'){
+              this.listfiltered=this.listCds;
+            }
+
+     }
+   }
 });
